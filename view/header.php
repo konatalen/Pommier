2e2b1ce94e6dcd566449d66be50b570eab53ad7d
Sullivan Chevallier <sullivan.chevallier@etud.univ-angers.fr>
2015-04-09 11:53:56 +0200

    ajout view

---
+++ view/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+	  <meta charset="utf-8">
+	  <title><?php echo $titre; ?></title>
+	  <link rel="stylesheet" href="../css/style.css">
+	  <link rel="icon" type="image/png" href="../images/favicon.jpg" />
+	</head>
+	<body>
+		
